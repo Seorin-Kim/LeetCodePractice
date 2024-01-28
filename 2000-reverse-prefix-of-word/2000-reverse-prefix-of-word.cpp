eafe444d7b8ec77e5001d1bb6ff2bdda4471aa0e
Seorin Kim <86909645+Seorin-Kim@users.noreply.github.com>
2024-01-28 17:33:25 +0900

    Time: 3 ms (43.60%), Space: 7.5 MB (8.97%) - LeetHub

---
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string reversePrefix(string word, char ch) {
+        int i;
+        for (i=0; i<word.length(); i++) {
+            if (word[i] == ch) {
+                break;
+            }
+        }
+
+        if (i == word.length()) {
+            return word;
+        }
+
+        std::reverse(word.begin(), word.begin()+i+1);
+        return word;
+    }
+};
