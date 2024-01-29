fefc9789ea23c974e0fac58109188a3cb63fcb81
Seorin Kim <86909645+Seorin-Kim@users.noreply.github.com>
2024-01-29 14:51:36 +0900

    Time: 4 ms (6.00%), Space: 7.3 MB (13.68%) - LeetHub

--- 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.cpp
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     string reversePrefix(string word, char ch) {
+        // word에서 ch의 인덱스 찾아서 i에 저장
         int i;
         for (i=0; i<word.length(); i++) {
             if (word[i] == ch) {
@@ -8,10 +9,12 @@ public:
             }
         }
 
+        // word에서 ch가 등장하지 않는 경우
         if (i == word.length()) {
             return word;
         }
 
+        // word에서 ch가 등장한 경우, 0부터 i까지 reverse (남은 칸은 자동으로 기존 word가 채워짐)
         std::reverse(word.begin(), word.begin()+i+1);
         return word;
     }
