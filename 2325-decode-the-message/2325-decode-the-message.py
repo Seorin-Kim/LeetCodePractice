0383cfd38efe83b7b7a01e4648c41fd0f4dc7eb9
Seorin Kim <86909645+Seorin-Kim@users.noreply.github.com>
2024-01-29 13:17:26 +0900

    Time: 45 ms (29.76%), Space: 16.6 MB (66.69%) - LeetHub

--- 2325-decode-the-message/2325-decode-the-message.py
+++ 2325-decode-the-message/2325-decode-the-message.py
@@ -1,13 +1,18 @@
 class Solution:
     def decodeMessage(self, key: str, message: str) -> str:
-        key_list = [i for i in key if i.isalpha()]
-        key_set = list(dict.fromkeys(key_list))
+        # key의 각 알파벳을 중복없이, 순서대로 key_set에 저장
+        key_set = []
+        for c in key:
+            if c.isalpha() and c not in key_set:
+                key_set.append(c)
 
+        # message를 key_set과 매칭하여 result에 저장
         result = []
         for c in message:
-            if c == ' ':
+            if c == ' ':    # 띄어쓰기 처리
                 result.append(' ')
-            else:
-                result.append(chr(key_set.index(c) + 97))
+            else:           # key_set에서의 인덱스 찾아서, 'a'의 유니코드에 더하기 -> 해당 알파벳의 유니코드
+                result.append(chr(key_set.index(c) + ord('a')))
 
+        # result의 각 원소를 문자열로 이어붙이기
         return ''.join(result)
