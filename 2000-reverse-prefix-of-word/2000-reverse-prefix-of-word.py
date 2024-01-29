667b0b50fcc8710b6b3a0f80777d06934eb88999
Seorin Kim <86909645+Seorin-Kim@users.noreply.github.com>
2024-01-29 13:09:54 +0900

    Time: 43 ms (21.05%), Space: 16.6 MB (60.91%) - LeetHub

--- 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.py
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.py
@@ -1,14 +1,17 @@
 class Solution:
     def reversePrefix(self, word: str, ch: str) -> str:
+        # ch가 word에 없는 경우
         if ch not in word:
             return word
         
+        # ch가 word에 있는 경우
         i = 0
         for c in word:
+            # ch가 등장하면 break -> i가 ch의 인덱스
             if c == ch:
                 break
             i += 1
-        
+        # i부터 거꾸로 + (i+1)부터 순서대로
         result = word[i::-1] + word[i+1:]
         return result
         
