011a2747327971dd715e957d74e7ee4a7061bf09
Seorin Kim <86909645+Seorin-Kim@users.noreply.github.com>
2024-01-31 15:27:03 +0900

    Time: 541 ms (99.00%), Space: 35.6 MB (80.13%) - LeetHub

--- 2433-find-the-original-array-of-prefix-xor/2433-find-the-original-array-of-prefix-xor.py
+++ 2433-find-the-original-array-of-prefix-xor/2433-find-the-original-array-of-prefix-xor.py
@@ -1,6 +1,6 @@
 class Solution:
     def findArray(self, pref: List[int]) -> List[int]:
-        # 첫번째 연산은 0^pref[0] 을 해야하므로, 제일 앞에 0. 추가
+        # 첫번째 연산은 0^pref[0] 을 해야하므로, 제일 앞에 0 추가
         pref.insert(0, 0)
 
         # pref 돌면서, 다음 것과 ^ 연산한 결과를 arr에 저장
