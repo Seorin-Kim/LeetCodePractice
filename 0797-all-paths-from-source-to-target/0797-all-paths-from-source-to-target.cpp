6a8cac1249793fd2aaaac9a3e4374f4dc594082f
Seorin Kim <86909645+Seorin-Kim@users.noreply.github.com>
2024-02-08 12:13:04 +0900

    Time: 9 ms (67.30%), Space: 14.1 MB (44.70%) - LeetHub

---
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    void dfs(vector<vector<int>>& graph, int curr, vector<int>& path, vector<vector<int>>& result) {
+        // 현재 노드가 (n-1) 노드이면, 찾은 path를 result에 추가
+        if (curr == graph.size()-1) {
+            // path의 복사본을 result에 추가 (이후에 path를 변경하면서 재사용할 것이기 때문)
+            vector<int> copyVec;
+            copyVec.resize((int)(path.size()));
+            copy(path.begin(), path.end(), copyVec.begin());
+            result.push_back(path);
+            return;
+        }
+        
+        // 현재 노드의 adj를 돌면서,
+        for (int i=0; i<graph[curr].size(); i++) {
+            path.push_back(graph[curr][i]);             // path에 방문한 노드 추가
+            dfs(graph, graph[curr][i], path, result);   // recursive하게 반복
+            path.erase(path.end()-1);                   // result에 추가한 path는 지우기
+        }
+        return;
+    }
+
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+        vector<vector<int>> result;
+        vector<int> path {0};
+        dfs(graph, 0, path, result);
+        return result;
+    }
+};
